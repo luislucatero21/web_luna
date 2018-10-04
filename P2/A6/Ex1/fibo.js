1f330cca0b9d41f568461cf4693e32836425fab9
Luis Lucatero <luislucatero21@gmail.com>
2018-10-03 19:51:23 -0500

    Actividad 6 ejercicio 1 completo

---
+++ P2/A6/Ex1/fibo.js
@@ -0,0 +1,13 @@
+var fibo_arr = [0, 1];
+
+function fibo(n){
+	var res;
+	if(n in fibo_arr) {
+		res = fibo_arr[n];
+	} else {
+		res = fibo(n - 1) + fibo(n - 2);
+	}
+	fibo_arr[n] = res;
+	return res;
+}
+
